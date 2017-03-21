1205216b5198c5fd22a7dfd54c0043734e6d5d49
Rod Johnson <rod@atomist.com>
2017-03-21 18:09:30 +1100

    Moved tests into test dir

--- test/test.ts
+++ test/test.ts
@@ -2,6 +2,10 @@
 
 import { suite, test, slow, timeout, skip, only } from "mocha-typescript";
 
+import {hello} from "@atomist/util/First"
+
 @suite class Hello {
-    @test "world"() { }
+    @test "world"() {
+      console.log(hello("Rod"))
+    }
 }
