ac02c3b2e4b6736741c4e89193d1770abbad8ea4
Rod Johnson <rod@atomist.com>
2017-03-27 14:35:33 +0100

    Testing PlanUtils

--- test/operations/PlanUtilsTest.ts
+++ test/operations/PlanUtilsTest.ts
@@ -2,15 +2,28 @@ import { suite, test, slow, timeout, skip, only } from "mocha-typescript";
 
 import {expect} from "chai"
 
-import * as query from "../../util/tree/QueryByExample"
+import * as ut from "../../operations/PlanUtils"
+
+import {UpgradeVersion} from "./UpgradeVersion"
 
 import {PathExpression} from "@atomist/rug/tree/PathExpression"
+import {Edit} from "@atomist/rug/operations/Handlers"
 
 @suite class PlanUtilsTest {
 
-   @test "plan utils"() {
-      let myString = "my/path"
-      let pe = new PathExpression(myString)
-      expect(pe.expression).to.equal(myString)
+   @test "edit with simple"() {
+     let uv = new UpgradeVersion
+     let projectName = "myproject"
+     uv.group = "Beatles"
+     uv.artifact = "foobar"
+     uv.desiredVersion = "3.0.4"
+      let e: Edit = ut.editWith(projectName, uv)
+      expect(e.kind).to.equal("edit")
+      expect(e.name).to.equal("UpgradeVersion")
+      expect(e.project).to.equal(projectName)
+      let params = e.parameters as any
+      expect(params.group).to.equal(uv.group)
+      expect(params.artifact).to.equal(uv.artifact)
+      expect(params.desiredVersion).to.equal(uv.desiredVersion)
     }
 }
---
+++ test/operations/UpgradeVersion.ts
@@ -0,0 +1,32 @@
+import {ProjectEditor,EditProject} from '@atomist/rug/operations/ProjectEditor'
+import {Project, Xml} from '@atomist/rug/model/Core'
+import { Pattern, RugOperation } from '@atomist/rug/operations/RugOperation'
+import {PathExpression,PathExpressionEngine,TextTreeNode} from '@atomist/rug/tree/PathExpression'
+import { Editor, Tags, Parameter } from '@atomist/rug/operations/Decorators'
+
+@Editor("UpgradeVersion", "Find and upgrade POM version")
+export class UpgradeVersion implements EditProject {
+
+    @Parameter({pattern: Pattern.group_id, description: "Group to match"})
+    group: string
+
+    @Parameter({pattern: Pattern.artifact_id, description: "Artifact to match"})
+    artifact: string
+
+    @Parameter({pattern: Pattern.semantic_version, description: "Version to upgrade to"})
+    desiredVersion: string
+    
+    edit(project: Project) {    
+
+        let eng: PathExpressionEngine = project.context().pathExpressionEngine();
+        let search = "some/path/expression"
+        eng.with<TextTreeNode>(project, search, version => {
+            if (version.value() != this.desiredVersion) {
+                //console.log(`Updated to desired version ${this.desiredVersion}`)
+                version.update(this.desiredVersion)
+            }
+        })
+    }
+}
+
+export const uv = new UpgradeVersion();
