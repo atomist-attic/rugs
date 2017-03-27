0ec38233d6c1585f873776c9ed337b2445c0a7b8
Rod Johnson <rod@atomist.com>
2017-03-27 14:56:09 +0100

    Added tests for QueryByExample

--- test/util/tree/QueryByExampleTest.ts
+++ test/util/tree/QueryByExampleTest.ts
@@ -4,13 +4,40 @@ import {expect} from "chai"
 
 import * as query from "../../../util/tree/QueryByExample"
 
+import {Build} from "@atomist/cortex/stub/Build"
+import {Repo} from "@atomist/cortex/stub/Repo"
+
 import {PathExpression} from "@atomist/rug/tree/PathExpression"
 
 @suite class QueryByExample {
 
-   @test "path expression should contain string"() {
-      let myString = "my/path"
-      let pe = new PathExpression(myString)
-      expect(pe.expression).to.equal(myString)
+   @test "simple node"() {
+      let b = new Build
+      let pathExpression = query.byExample(b)
+      expect(pathExpression.expression).to.equal("/Build()")
+    }
+
+    @test "node with simple property predicate"() {
+      let b = new Build().withBuildType("mybuild")
+      let pathExpression = query.byExample(b)
+      expect(pathExpression.expression).to.equal(`/Build()[@buildType='${b.buildType()}']`)
+    }
+
+    @test "node with two simple property predicates"() {
+      let b = new Build().withBuildType("mybuild").withStatus("failed")
+      let pathExpression = query.byExample(b)
+      expect(pathExpression.expression).to.equal(`/Build()[@buildType='${b.buildType()}'][@status='${b.status()}']`)
+    }
+
+    @test "node with related node"() {
+      let b = new Build().withBuildType("mybuild").withOn(new Repo())
+      let pathExpression = query.byExample(b)
+      expect(pathExpression.expression).to.equal(`/Build()[@buildType='${b.buildType()}'][/on::Repo()]`)
+    }
+
+    @test "node with related node but in match"() {
+      let b = new Build().withBuildType("mybuild").withOn(query.match(new Repo()))
+      let pathExpression = query.byExample(b)
+      expect(pathExpression.expression).to.equal(`/Build()[@buildType='${b.buildType()}']/on::Repo()`)
     }
 }
