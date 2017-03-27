1c07b9e8314d4359a15a0e362fc139f168b9f774
Rod Johnson <rod@atomist.com>
2017-03-27 10:54:45 +0100

    Tests

--- test/test.ts
+++ test/test.ts
@@ -2,7 +2,7 @@
 
 import { suite, test, slow, timeout, skip, only } from "mocha-typescript";
 
-import {hello} from "@atomist/util/First"
+import {hello} from "../@atomist/util/First"
 
 @suite class Hello {
     @test "world"() {
