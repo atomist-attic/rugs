1944da236163b661d926869a2827e8a125fbeb7a
Rod Johnson <rod@atomist.com>
2017-03-27 11:23:36 +0100

    Added chai

--- test/test.ts
+++ test/test.ts
@@ -2,10 +2,21 @@
 
 import { suite, test, slow, timeout, skip, only } from "mocha-typescript";
 
+import {expect} from "chai"
+
 import {hello} from "../util/First"
+
+import {PathExpression} from "@atomist/rug/tree/PathExpression"
 
 @suite class Hello {
     @test "world"() {
       console.log(hello("Rod"))
+    }
+
+    @test "path expression"() {
+      let myString = "my/path"
+      let pe = new PathExpression(myString)
+      console.log(pe)
+      expect(pe.expression).to.equal(myString)
     }
 }
--- util/First.ts
+++ util/First.ts
@@ -1,4 +1,9 @@
 
+import {PathExpression} from "@atomist/rug/tree/PathExpression"
+
 export function hello(s: string) {
+  let myString = "my/path"
+  let pe = new PathExpression(myString)
+  
   return "hello" + s
 }
