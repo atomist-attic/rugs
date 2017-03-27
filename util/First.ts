381e6a160ed9209576b0edcc35d91665ab4a79aa
Rod Johnson <rod@atomist.com>
2017-03-27 11:38:01 +0100

    Cleanup after running tests in subdirectories

--- util/First.ts
+++
@@ -1,9 +0,0 @@
-
-import {PathExpression} from "@atomist/rug/tree/PathExpression"
-
-export function hello(s: string) {
-  let myString = "my/path"
-  let pe = new PathExpression(myString)
-  
-  return "hello" + s
-}
