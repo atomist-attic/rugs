ce25072c370f4072571d5dacbdc78f61a827f6ad
Christian Dupuis <cd@atomist.com>
2017-03-29 11:06:41 +0100

    Fix compile errors, left test failures

--- test/util/tree/QueryByExampleTest.ts
+++ test/util/tree/QueryByExampleTest.ts
@@ -1,13 +1,13 @@
-import { suite, test, slow, timeout, skip, only } from "mocha-typescript";
+import { suite, test, slow, timeout, skip, only } from "mocha-typescript"
 
-import {expect} from "chai"
+import { expect } from "chai"
 
 import * as query from "../../../util/tree/QueryByExample"
 
-import {Build} from "@atomist/cortex/stub/Build"
-import {Repo} from "@atomist/cortex/stub/Repo"
+import { Build } from "@atomist/cortex/stub/Build"
+import { Repo } from "@atomist/cortex/stub/Repo"
 
-import {PathExpression} from "@atomist/rug/tree/PathExpression"
+import { PathExpression } from "@atomist/rug/tree/PathExpression"
 
 @suite class QueryByExample {
 
@@ -18,26 +18,26 @@ import {PathExpression} from "@atomist/rug/tree/PathExpression"
     }
 
     @test "node with simple property predicate"() {
-      let b = new Build().withBuildType("mybuild")
+      let b = new Build().withType("mybuild")
       let pathExpression = query.byExample(b)
-      expect(pathExpression.expression).to.equal(`/Build()[@buildType='${b.buildType()}']`)
+      expect(pathExpression.expression).to.equal(`/Build()[@type='${b.type()}']`)
     }
 
     @test "node with two simple property predicates"() {
-      let b = new Build().withBuildType("mybuild").withStatus("failed")
+      let b = new Build().withType("mybuild").withStatus("failed")
       let pathExpression = query.byExample(b)
-      expect(pathExpression.expression).to.equal(`/Build()[@buildType='${b.buildType()}'][@status='${b.status()}']`)
+      expect(pathExpression.expression).to.equal(`/Build()[@type='${b.type()}'][@status='${b.status()}']`)
     }
 
     @test "node with related node"() {
-      let b = new Build().withBuildType("mybuild").withOn(new Repo())
+      let b = new Build().withType("mybuild").withOn(new Repo())
       let pathExpression = query.byExample(b)
-      expect(pathExpression.expression).to.equal(`/Build()[@buildType='${b.buildType()}'][/on::Repo()]`)
+      expect(pathExpression.expression).to.equal(`/Build()[@type='${b.type()}'][/on::Repo()]`)
     }
 
     @test "node with related node but in match"() {
-      let b = new Build().withBuildType("mybuild").withOn(query.match(new Repo()))
+      let b = new Build().withType("mybuild").withOn(query.match(new Repo()))
       let pathExpression = query.byExample(b)
-      expect(pathExpression.expression).to.equal(`/Build()[@buildType='${b.buildType()}']/on::Repo()`)
+      expect(pathExpression.expression).to.equal(`/Build()[@type='${b.type()}']/on::Repo()`)
     }
 }
