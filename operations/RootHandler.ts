bdcd702758080de5f57224dcf481408345fd1b89
Rod Johnson <rod@atomist.com>
2017-03-30 11:20:15 +0100

    Updated to work with rug 0.21.0

--- operations/RootHandler.ts
+++ operations/RootHandler.ts
@@ -1,17 +1,21 @@
 
 import { Match, GraphNode } from "@atomist/rug/tree/PathExpression";
-import { Plan, Message, HandleEvent } from "@atomist/rug/operations/Handlers";
+import { Plan, HandleEvent } from "@atomist/rug/operations/Handlers";
 
 /**
  * Convenient event handler superclass when we're only interested in the root
- * match
+ * match. This is the commonest case.
  */
-export abstract class RootHandler<R extends GraphNode> implements HandleEvent<R,R> {
+export abstract class RootHandler<R extends GraphNode> implements HandleEvent<R, R> {
 
-     handle(m: Match<R,R>): Plan | Message {
-        return this.onMatch(m.root());
-      }
+  handle(m: Match<R, R>): Plan {
+    return this.onMatch(m.root());
+  }
 
-      abstract onMatch(root: R): Plan | Message;
+  /**
+   * Handle the given root match
+   * @param root root node of the match 
+   */
+  abstract onMatch(root: R): Plan;
 
 }
