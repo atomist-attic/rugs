52cc6df52913bdf7c50600ca787965800d43c699
David Dooling <david@atomist.com>
2017-04-11 00:26:14 -0500

    Alter dependency handling
    Install latest @atomist module dependencies from snapshot repository
    unless this is a release build.  Set specific versions of @atomist
    module dependencies in package.json.
    
    Downgrade to Node 6 since all other modules use it.

--- npm-publish.bash
+++ npm-publish.bash
@@ -3,7 +3,7 @@
 set -o pipefail
 
 declare Pkg=npm-publish
-declare Version=0.1.0
+declare Version=0.2.0
 
 function msg() {
     echo "$Pkg: $*"
@@ -16,50 +16,55 @@ function err() {
 function main() {
     msg "branch is ${TRAVIS_BRANCH}"
     local module_version=$1
-    local package="package.json"
-    local tmp_package="$package.tmp"
     if [[ ! $module_version ]]; then
         err "first parameter must be the version number of the module to publish"
         return 10
     fi
+    shift
 
+    local package="package.json"
+    local tmp_package="$package.tmp"
     if ! jq --arg tag "$TRAVIS_TAG" '.version = $tag' "$package" > "$tmp_package"; then
         err "failed to set version in $package"
         return 1
     fi
-    cp "$tmp_package" "$package"
+    if ! mv "$tmp_package" "$package"; then
+        err "failed to overwrite $package"
+        return 1
+    fi
 
      # npm honors this
     rm -f "$target/.gitignore"
 
-    if [[ $TRAVIS_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-        msg "Creating local .npmrc using API key from environment"
-        if ! ( umask 077 && echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > "$HOME/.npmrc" ); then
-            err "failed to create $HOME/.npmrc"
-            return 1
-        fi
-        if ! ( cd "$target" && npm publish --access=public ); then
-            err "failed to publish node module"
-            cat "$target/npm-debug.log"
-            return 1
-        fi
-    else [[ $TRAVIS_BRANCH == "master" ]]; then
-        msg "Creating local .npmrc from details generated in Artifactory..."
-        if [[ $ATOMIST_REPO_TOKEN  && $ATOMIST_REPO_USER ]]; then
-            if ! ( curl -u"${ATOMIST_REPO_USER}":"${ATOMIST_REPO_TOKEN}" "https://atomist.jfrog.io/atomist/api/npm/auth" >  "$HOME/.npmrc"); then
+    local registry
+    if [[ $module_version =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        if [[ $NPM_TOKEN ]]; then
+            msg "creating local .npmrc using NPM token from environment"
+            if ! ( umask 077 && echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > "$HOME/.npmrc" ); then
                 err "failed to create $HOME/.npmrc"
                 return 1
             fi
         else
-            err "Could not find credentials for publish to Artifactory"
+            msg "assuming your .npmrc is setup correctly for this project"
+        fi
+    elif [[ $TRAVIS_BRANCH == "master" ]]; then
+        msg "creating local .npmrc from details generated in Artifactory..."
+        if [[ $ATOMIST_REPO_TOKEN  && $ATOMIST_REPO_USER ]]; then
+            if ! ( umask 077 && curl -u"${ATOMIST_REPO_USER}":"${ATOMIST_REPO_TOKEN}" "https://atomist.jfrog.io/atomist/api/npm/auth" >  "$HOME/.npmrc"); then
+                err "failed to create $HOME/.npmrc"
+                return 1
+            fi
+        else
+            err "could not find credentials for publish to Artifactory"
             return 1
         fi
+        registry=--registry=https://atomist.jfrog.io/atomist/api/npm/npm-dev-local
+    fi
 
-        if ! ( cd "$target" && npm publish --registry https://atomist.jfrog.io/atomist/api/npm/npm-dev-local --access=public ); then
-            err "failed to publish node module to artifactory"
-            cat "$target/npm-debug.log"
-            return 1
-        fi
+    if ! ( cd "$target" && npm publish --access=public $registry ); then
+        err "failed to publish node module to artifactory"
+        cat "$target/npm-debug.log"
+        return 1
     fi
 }
 
--- update-deps.bash
+++ update-deps.bash
@@ -3,7 +3,7 @@
 set -o pipefail
 
 declare Pkg=update-deps
-declare Version=0.1.0
+declare Version=0.2.0
 
 function msg() {
     echo "$Pkg: $*"
@@ -16,28 +16,16 @@ function err() {
 function main() {
     msg "branch is ${TRAVIS_BRANCH}"
 
-    if [[ $TRAVIS_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-        msg "Updating @atomist modules to latest released versions..."
-        if ! npm install @atomist/rug@latest -S; then
-            err "failed to install latest @atomist/rug module"
-            return 1
-        fi
-        if ! npm install @atomist/cortex@latest -S; then
-            err "failed to install latest @atomist/cortex module"
-            return 1
-        fi
-    elif [[ $TRAVIS_BRANCH == "master" ]]; then
-        msg "Updating @atomist modules to latest development versions..."
-        if ! npm install @atomist/rug@latest -S --registry https://atomist.jfrog.io/atomist/api/npm/npm-dev; then
-            err "Failed to install latest @atomist/rug from https://atomist.jfrog.io/atomist/api/npm/npm-dev"
-            return 1
-        fi
-        if ! npm install @atomist/cortex@latest -S --registry https://atomist.jfrog.io/atomist/api/npm/npm-dev; then
-            err "Failed to install latest @atomist/cortex from https://atomist.jfrog.io/atomist/api/npm/npm-dev"
-            return 1
-        fi
-    else
-        msg "Not a release or master build => doing nothing to package.json"
+    if ! [[ $TRAVIS_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        msg "updating @atomist modules to latest development versions"
+        local registry=https://atomist.jfrog.io/atomist/api/npm/npm-dev
+        local module
+        for module in rug cortex; do
+            if ! npm install "@atomist/$module@latest" --save --registry="$registry"; then
+                err "Failed to install latest @atomist/$module from $registry"
+                return 1
+            fi
+        done
     fi
 }
 
